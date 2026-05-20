79ac41b8af5f1ba7ade3e38e4dfb39dee74f7795
da5905p <da5905p@greenwich.ac.uk>
2026-05-20 15:41:19 +0100

    Updates

--- src/components/MainHeading.tsx
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.css";
-
-function MainHeading () {
-    <div>
-        back
-    </div>
-}
-
-export default MainHeading;
--- src/pages/Services.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Services: React.FC = () => {
-  return <div>Services Page</div>;
-};
-
-export default Services;
